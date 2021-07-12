c3857b5ee9873386deecac8afe262c31abebd05c
Harvey <harvey0379@163.com>
2021-07-12 14:59:06 +0800

    feat: demo

---
+++ src/main/java/HelloSelenium.java
@@ -0,0 +1,24 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class HelloSelenium {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        try {
+            driver.get("https://www.baidu.com");
+            driver.findElement(By.name("wd")).sendKeys("cheese" + Keys.ENTER);
+            WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+            System.out.println(firstResult.getAttribute("textContent"));
+        } finally {
+            driver.quit();
+        }
+    }
+}
